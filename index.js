10724b6976f5d59c89ea23a02c82ede8455ae159
sohidull <mdsohidulislam@gmail.com>
2020-08-28 10:13:30 +0600

    minnanonihongo

---
+++ index.js
@@ -0,0 +1,292 @@
+// Lesson 1 Grammar
+
+// I'm md sohidul islam.
+// I'm not md sohidul islam.
+// I'm Bangladeshi.
+// I'm not Bangladeshi.
+// Ms.mariya is a teacher.
+// Ms.mariy is not a teacher.
+// Is Ms.mariya a teacher?
+// Yes,Ms.mariya is a teacher.
+// No,Ms.mariya is not a teacher.
+// Is that person Andrea.
+// Yes,that person is Andrea.
+// No, that person is not Andrea.
+// Who is that person?
+// That person is md sohidul islam.
+// James is an engineer.
+// Mariya is also an engineer.
+// Me too.
+// Andrea is an IBM employee.
+// James is a student of manila university.
+// This is Mr.Andrea.That person is Ms.mariya.
+// How old are you?(Ms.mariya);
+
+// Lesson 2 Grammar
+
+// That person over there is a student of university of the Philippines.
+// This is a book.
+// That is a Umbrella.
+// That is a notebook.(over there).
+// Is this a book?
+// Is that a umbrella?
+// Is that a notebook?
+// What is this?
+// What is that?
+// What is that thing over there?
+// This umbrella is mine.
+// That book is mine too.
+// Who is that person over there?
+// Is that a dictionary?
+// Yes, it is.
+// No,it is not.
+// Is it a 4 or 9?
+// Is it black or white.
+// Book on computers.
+// Book about cameras.
+// Book about what?
+// My book.
+// Andrea's desk.
+// This is my book on computers.
+// Whose bag is that?
+// It's ms.mariya's.
+// Is this bag yours?
+// No,It's not mine.
+// Is Andrea an IBM employee?
+// Yes,he is an IBM employee.iS this coffee yours?
+// No,it's mariya's.
+
+// Lesson 3 grammar
+
+// The restroom is over there.
+// The computer section in on the first floor.
+// Ms.mariya is in the conference room.
+// Where are you from(what country).
+// What is the name of your school?
+// What company do you work for?
+// Bangladeshi shoes.
+// Apple smartphone.
+// Where is this bag made ?/Who is the maker of this bag?
+// IMB's employee.
+// Where are you from?
+// What's your name?
+// How old are you?
+// mm: Excuse me,Where is the camera section?
+// nn: It's in the 3rd floor.
+// mm: Excuse me, Could you show me that camera?
+// nn: Yes, Here you are.
+// mm: Is this made in china?
+// nn: No,It's made in japan.
+// mm: How much is it?
+// nn: 343 yen.
+// mm: well then,I'll take it.
+
+
+// Lesson 4 grammar
+
+// It's eight fifteen now /It's 8 15 now.
+// What time is it now ?
+// What time is it in Bangladesh ?
+// I study everyday.
+// I sleep at ten.
+// I'll work on April 1st.
+// What time do you sleep.
+// I wake up at six everyday.
+// I'll wake up at six tomorrow.
+// I don't wake up at six everyday.
+// I won't wake up at six tomorrow.
+// I woke up at six this morning.
+// I didn't study everyday.
+// Do you study everyday?
+// Yes, I do.
+// NO, I don't.
+// Do you take a day off yesterday?
+// Yes, I did.
+// I work from 8 to 4.
+// I work from Monday to Friday.
+// It takes 4 hours from Dhaka to Rajshahi.
+// I work from 9.
+// To boyaliya please.
+// I'm from osaka/I came from osaka.
+// The bank is (open) from 9 to 3.
+// Lunch break is until 1:30.
+// From what time until what time is the Lunch break?
+// I don't work on Saturdays and Sundays.
+// Banks are Closed on Fridays and Saturdays.
+// It's a holiday tomorrow, right?
+// It's hot, isn't ?
+// It's cold isn't ?
+// That must be tough.
+// Isn't it amazing.
+
+// /Lesson 5 grammar
+
+// I'll go to supermarket.
+// I came to japan last year.
+// I'll go back home next week's.
+// Where are you going ?
+// Where are you going next week.
+// I am not going anywhere.
+// Where did  you go last week?
+// I didn't go anywhere.
+// I'll go by train.
+// I came by taxi.
+// I went home from school on foot.
+// How do you go to his house?By bicycle.
+// She goes to school with maria .
+// He went to germany with his family.
+// Who did you go to London with?
+// By myself/alone.
+// When are your family coming?=>>On December 25th.
+// When will you go back?=>>I'll gi back next year.
+// When is your birthday? =>>On January 3rd.
+// Hey, I'm going.
+// It is.
+// Hey, It's delicous.
+
+
+
+// পাঠ 1 ব্যাকরণ
+
+// আমি মোঃ সোহিদুল ইসলাম।
+// আমি এমডি সোহিদুল ইসলাম নই।
+// আমি বাংলাদেশী।
+// আমি বাংলাদেশী নই।
+// মেস.মারিয়া একজন শিক্ষক।
+// Ms.mary কোন শিক্ষক নন।
+// মেস.মারিয়া কি একজন শিক্ষক?
+// হ্যাঁ, মিস.মারিয়া একজন শিক্ষক।
+// না, মিস.মারিয়া কোনও শিক্ষক নন।
+// সেই ব্যক্তি কি আন্দ্রেয়া?
+// হ্যাঁ, সেই ব্যক্তিটি আন্দ্রেয়া a
+// না, সেই ব্যক্তিটি আন্দ্রে নয়।
+// সেই ব্যক্তি কে?
+// সেই ব্যক্তি হলেন মোঃ সোহিদুল ইসলাম।
+// জেমস একজন প্রকৌশলী।
+// মারিয়াও ইঞ্জিনিয়ার।
+// আমিও.
+// আন্দ্রেয়া একজন আইবিএম কর্মচারী।
+// জেমস ম্যানিলা বিশ্ববিদ্যালয়ের ছাত্র।
+// তিনি মিঃ আন্ড্রেয়া hat এই ব্যক্তি হলেন সুশ্রীমারিয়া।
+// আপনার বয়স কত? (সুশ্রীমারিয়া);
+
+// পাঠ 2 ব্যাকরণ
+
+// সেখানকার ওই ব্যক্তি ফিলিপাইনের বিশ্ববিদ্যালয়ের শিক্ষার্থী।
+// এটি একটি বই.
+// এটি একটি ছাতা।
+// এটি একটি নোটবুক।
+// এটি কি একটি বই?
+// এটা কি ছাতা?
+// এটি কি একটি নোটবুক?
+// এটা কি?
+// এটা কি?
+// সেখানে কি জিনিস?
+// এই ছাতা আমার।
+// সেই বইটিও আমার।
+// ওখানে সেই ব্যক্তি কে?
+// এটি কি অভিধান?
+// হ্যাঁ, তাই
+// না এটা না.
+// এটি একটি 4 বা 9?
+// এটি কি কালো বা সাদা।
+// কম্পিউটারে বুক করুন।
+// ক্যামেরা সম্পর্কে বই।
+// কি সম্পর্কে বই?
+// আমার বই.
+// আন্ড্রেয়ার ডেস্ক
+// এটি কম্পিউটারে আমার বই।
+// এটা কার থলে?
+// এটি এমএস.মারিয়া এর।
+// এই ব্যাগটি কি আপনার?
+// না, এটা আমার না.
+// আন্দ্রেয়া কি একজন আইবিএম কর্মচারী?
+// হ্যাঁ, তিনি একজন আইবিএম কর্মচারী i এই কফি আপনার?
+// না, এটা মারিয়ার।
+
+// পাঠ 3 ব্যাকরণ
+
+// বিশ্রামখানা সেখানে শেষ।
+// প্রথম তলায় কম্পিউটার বিভাগ।
+// মিসেসমারিয়া কনফারেন্স রুমে আছেন।
+// আপনি কোথা থেকে (কোন দেশ)?
+// তোমার স্কুলের নাম কি?
+// তুমি কোন কোম্পানির জন্য কাজ কর?
+// বাংলাদেশী জুতো।
+// অ্যাপল স্মার্টফোন।
+// এই ব্যাগটি কোথায় তৈরি করা হয়েছে? / এই ব্যাগ প্রস্তুতকারী কে?
+// আইএমবির কর্মচারী।
+// তুমি কোথা থেকে আসছো?
+// তোমার নাম কি?
+// আপনার বয়স কত?
+// মিমি: মাফ করবেন, ক্যামেরা বিভাগটি কোথায়?
+// এনএন: এটি তৃতীয় তলায়।
+// মিমি: মাফ করবেন, আপনি আমাকে ক্যামেরাটি দেখাতে পারবেন?
+// এনএন: হ্যাঁ, আপনি এখানে আছেন।
+// মিমি: এটি কি চীনায় তৈরি?
+// এনএন: না, এটি জাপানে তৈরি।
+// মিমি: এটা কত?
+// nn: 343 ইয়েন
+// মিমি: ঠিক আছে, তাহলে আমি এটি গ্রহণ করব।
+
+// পাঠ 4 ব্যাকরণ
+
+// এটি এখন আট পনেরো / এখন 8 15।
+// এখন ক 'টা বাজে ?
+// বাংলাদেশে এটা কত সময়?
+// আমি প্রতিদিন অধ্যয়ন করি.
+// আমি দশটায় ঘুমাই।
+// আমি ১ লা এপ্রিল কাজ করব।
+// কি সময় আপনি ঘুম না.
+// আমি প্রতিদিন ছয়টায় উঠেছি
+// আগামীকাল ছয়টায় ঘুম থেকে উঠব।
+// আমি প্রতিদিন ছয়টায় উঠি না।
+// আমি আগামীকাল ছয়টায় উঠব না।
+// আমি আজ সকালে ছয়টায় ঘুম থেকে উঠি।
+// আমি প্রতিদিন পড়াশোনা করিনি।
+// আপনি কি প্রতিদিন পড়াশুনা করেন?
+// হ্যাঁ আমি কি.
+// না, আমি না।
+// তুমি কি গতকাল একদিন ছুটি নেবে?
+// হ্যা, আমি করেছিলাম.
+// আমি 8 থেকে 4 পর্যন্ত কাজ করি।
+// আমি সোমবার থেকে শুক্রবার পর্যন্ত কাজ করি।
+// Dhakaাকা থেকে রাজশাহী যেতে 4 ঘন্টা সময় লাগে।
+// আমি 9 থেকে কাজ করি।
+// দয়া করে বয়লিয়াকে।
+// আমি ওসাকা থেকে / আমি ওসাকা থেকে এসেছি।
+// ব্যাংকটি 9 থেকে 3 পর্যন্ত (উন্মুক্ত) রয়েছে।
+// মধ্যাহ্নভোজন 1:30 টা অবধি।
+// কোন সময় থেকে কোন সময় পর্যন্ত মধ্যাহ্নভোজন বিরতি?
+// আমি শনি ও রবিবার কাজ করি না।
+// ব্যাংকগুলি শুক্র ও শনিবার বন্ধ থাকে।
+// আগামীকাল ছুটি, তাই না?
+// গরম আছে, তাই না?
+// ঠান্ডা লাগছে না?
+// এটা অবশ্যই শক্ত হতে হবে।
+// আশ্চর্য কি না?
+
+// / পাঠ 5 ব্যাকরণ
+
+// আমি সুপার মার্কেটে যাব।
+// গত বছর আমি জাপানে এসেছি।
+// আমি পরের সপ্তাহে বাড়িতে ফিরে যাব।
+// আপনি কোথায় যাচ্ছেন ?
+// আপনি পরের সপ্তাহে কোথায় যাচ্ছেন?
+// আমি কোথাও যাচ্ছি না।
+// যেখানে আপনি গত সপ্তাহে গিয়েছিলে?
+// আমি কোথাও যাইনি।
+// আমি ট্রেনে যাব।
+// আমি ট্যাক্সি দিয়ে এসেছি।
+// আমি স্কুল থেকে পায়ে হেঁটে বাড়ি গেলাম।
+// আপনি কীভাবে তাঁর বাসায় যাবেন?
+// সে মারিয়া নিয়ে স্কুলে যায়।
+// তিনি পরিবারের সাথে জার্মানিতে গেছেন।
+// আপনি কার সাথে লন্ডনে গিয়েছিলেন?
+// আমার দ্বারা / একা।
+// আপনার পরিবার কখন আসবে? = >> ২৫ শে ডিসেম্বর।
+// আপনি কখন ফিরে যাবেন? = >> আমি পরের বছর ফিরে আসব।
+// তোমার জন্মদিন কবে? = >> ২ য় জানুয়ারী।
+// আরে, আমি যাচ্ছি
+// এইটা.
+// আরে, এটি সুস্বাদু।
---
+++ vocabulary.js
@@ -0,0 +1,92 @@
+// Lesson 1 Vocabulary
+
+// I
+// We 
+// You 
+// That person,he,she
+// Ladies and gentlemen, all of You
+// Ms,Mr
+// Teacher,instructor
+// Teacher,instructor
+// Student
+// Company employee
+// Employee of__Company
+// Bank employee
+// Medical doctor
+// Researcher,Scholar
+// Engineer
+// University
+// Hospital
+// Electricity,Light
+// Who
+// ___years old
+// How old
+// Yes
+// no
+// Excuse me,but
+// May I have your name?
+// How do you do?
+// Pleased to meet you.
+// To me
+// This is mr.sohidul islam
+// I came from Chapainawabgonj
+// U.S.A
+// U.K
+// INDIA
+// Indonesia
+// South Korea
+// Thailand
+// China
+// Germany
+// Japan
+// France
+// Brazil
+
+// Lesson 2 Vocabulary
+
+// This,
+// That,
+// That
+// This
+// That
+// That
+// Book
+// Dictionary
+// Magazine
+// Newspaper
+// Notebook
+// Pocket Notebook
+// Business card
+// card
+// Telephone card
+// Pencil
+// Ballpoint pen
+// Mechanical pencil, Propelling Pencil
+// Key
+// Watch,clock
+// Umbrella
+// Bag,Briefcase
+// [Cassette]tape
+// tape-recorder
+// Television
+// Radio
+// Camera
+// Computer
+// Automobile,car
+// Desk
+// Chair
+// Chocolate
+// Coffee
+// The English Language
+// The japanese Language
+// What
+// So
+// No it isn't/You are wrong
+// I see/Is that so?
+// Well
+// It's nothing/It's a token of my gratitude,
+// Please/Here you are.
+// Well/thanks
+// Thank you[very much]
+
+
